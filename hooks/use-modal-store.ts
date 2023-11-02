c675866800d48deebfe255571b0f0d0409e8227a
Alex Rossov <poccob86@gmail.com>
2023-11-02 12:19:29 +0800

    add leave members servers

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,8 @@
 import { create } from 'zustand';
 import { Server } from '@prisma/client';
 
-export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members' | 'createChannel';
+export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members' | 'createChannel'
+  | 'leaveServer';
 
 interface ModalData {
   server?: Server
