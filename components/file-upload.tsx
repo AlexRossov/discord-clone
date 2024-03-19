5efa56d526ce3cdcdd72de6128a041560a8eeea8
Alex Rossov <poccob86@gmail.com>
2024-03-19 12:51:37 +0800

    add chat component

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
-//'use client';
+'use client';
 
-import { X } from 'lucide-react';
+import { FileIcon, X } from 'lucide-react';
 import Image from 'next/image';
 
 import { UploadDropzone } from '@/lib/uploadthing';
@@ -10,7 +10,7 @@ import '@uploadthing/react/styles.css';
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
-  endpoint: 'messageFile' | 'serverImage';
+  endpoint: 'messageFile' | 'serverImage'
 }
 
 export const FileUpload = ({
@@ -23,13 +23,42 @@ export const FileUpload = ({
   if (value && fileType !== 'pdf') {
     return (
       <div className="relative h-20 w-20">
-        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full"
+        />
         <button
-          className="bg-rose-500 text-white rounded-full absolute top-0 right-0 shadow-sm"
-          type="button"
           onClick={() => onChange('')}
+          className="bg-rose-500 text-white p-1 rounded-full absolute
+          top-0 right-0 shadow-sm"
+          type="button"
         >
-          <X className="h-6 w-6" />
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === 'pdf') {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange('')}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
         </button>
       </div>
     );
@@ -37,11 +66,13 @@ export const FileUpload = ({
 
   return (
     <UploadDropzone
-      endpoint = { endpoint }
-      onClientUploadComplete = { (res) => {
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
         onChange(res?.[0].url);
       }}
-      onUploadError = {(error: Error) => { console.log(error); }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
     />
   );
 };
